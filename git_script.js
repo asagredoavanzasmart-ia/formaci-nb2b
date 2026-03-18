7407f98104f7a663a88264d4219580ca0df9f02d
asagredoavanzasmart-ia <asagredoavanzasmart-ia@users.noreply.github.com>
2026-03-18 09:30:27 -0300

    Updates

--- git_script.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-try {
-  const output = execSync('git log -p src/components/SlideCRMPipeline.tsx').toString();
-  fs.writeFileSync('gitlog.txt', output);
-} catch (e) {
-  fs.writeFileSync('gitlog.txt', e.toString());
-}
--- test_server.cjs
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Server is working\n');
-});
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Test server running at http://0.0.0.0:3000/');
-});
